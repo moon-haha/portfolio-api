30626d88b1557907b2ddea1ab24a19048d705549
andreas <andshmoon@gmail.com>
2023-01-10 11:50:29 +0900

    created vue app

--- README.md
+++ README.md
@@ -3,7 +3,21 @@
 ---
 
 - [x] express server on
+- [x] Created Vue App
 - [ ] REST API
+  - [ ] GET posts
+  - [ ] GET post
+  - [ ] POST post
+  - [ ] PUT post
+  - [ ] DELETE posts
 - [ ] Serverless Deploy
 - [ ] Auth
+  - [ ] MongoDB-Session
 - [ ] MongoDB Connect
+
+# 프론트엔드
+
+---
+
+- [ ] vue with bootstrap
+- [ ] vuex
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
+//body-parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(8080, function () {
-  console.log("listening on 8080");
+  console.log('listening on 8080');
 });
 
-app.get("/", function (req, res) {
-  res.send("Hello");
+app.get('/', function (req, res) {
+  res.send('Hello');
+});
+
+app.post('/add', function (req, res) {
+  res.send(req.body);
 });
