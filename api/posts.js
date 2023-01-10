8e4be2c06b6f1b5143645bd86103566825ebca13
andreas <andshmoon@gmail.com>
2023-01-10 21:18:23 +0900

    post counter

--- api/posts.js
+++ api/posts.js
@@ -28,32 +28,50 @@ posts.get('/posts/:id', function (req, res) {
     console.log('mongo connected');
 
     databaseName = result.db('todoapp');
-    // databaseName
-    //   .collection('post')
-    //   .insertOne({ 이름: 'Jhon', 나이: 20 }, function (err, result) {
-    //     console.log('완료');
-    //   });
+    databaseName
+      .collection('post')
+      .find()
+      .toArray(function (err, result) {
+        res.send(result);
+      });
   });
-
   res.send(`GET posts ${req.params.id}`);
 });
 // - [ ] POST posts by :id
 posts.post('/posts', function (req, res) {
-  //name, age
   MongoClient.connect(MongoURI, function (err, result) {
     if (err) return console.log(err);
     console.log('mongo connected');
-
     databaseName = result.db('todoapp');
+
     databaseName
-      .collection('post')
-      .insertOne(
-        { 이름: req.body.name, 나이: req.body.age },
-        function (err, result) {
-          console.log('완료');
-          res.send(`POST posts ${req.body.name}`);
-        },
-      );
+      .collection('counter')
+      .findOne({ name: '게시물갯수' }, function (에러, 결과) {
+        console.log(결과);
+        var 총게시물갯수 = 결과.totalPost;
+
+        databaseName.collection('post').insertOne(
+          {
+            _id: 총게시물갯수 + 1,
+            name: req.body.name,
+            age: req.body.age,
+          },
+          function (에러, 결과) {
+            databaseName
+              .collection('counter')
+              .updateOne(
+                { name: '게시물갯수' },
+                { $inc: { totalPost: 1 } },
+                function (에러, 결과) {
+                  if (에러) {
+                    return console.log(에러);
+                  }
+                  res.send(req.body);
+                },
+              );
+          },
+        );
+      });
   });
 });
 // - [ ] PUT post by :id
