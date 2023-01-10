ac79e8a463dcec353315db94a7361e0665cbf3da
andreas <andshmoon@gmail.com>
2023-01-10 12:47:32 +0900

    mongodb connected

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
@@ -5,12 +7,29 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//database
+const MongoClient = require('mongodb').MongoClient;
+var databaseName;
+// eslint-disable-next-line no-undef
+const MongoURI = process.env.MONGO_URI;
+MongoClient.connect(MongoURI, function (err, result) {
+  if (err) return console.log(err);
+
+  databaseName = result.db('todoapp');
+
+  databaseName
+    .collection('post')
+    .insertOne({ 이름: 'Jhon', 나이: 20 }, function (err, result) {
+      console.log('완료');
+    });
+
+  app.listen(8080, function () {
+    console.log('listening on 8080 and connected db');
+  });
+});
+
 //import routes /api/posts
 app.use('/api/', require('./api/posts.js'));
-
-app.listen(8080, function () {
-  console.log('listening on 8080');
-});
 
 app.get('/', function (req, res) {
   res.send('Hello');
