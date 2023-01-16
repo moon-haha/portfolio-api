634792870c75e18367b646aae4b649b7f5e7e8a1
andreas <andshmoon@gmail.com>
2023-01-16 10:39:04 +0900

    mongodb connect middleware

--- server.js
+++ server.js
@@ -9,16 +9,6 @@ const MongoClient = require('mongodb').MongoClient;
 // eslint-disable-next-line no-undef
 const MongoURI = process.env.MONGO_URI;
 
-//body-parser
-app.use(express.json());
-app.use(
-  cors({
-    origin: '*', // 출처 허용 옵션
-    credential: 'true', // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
-  }),
-);
-app.use(express.urlencoded({ extended: true }));
-
 //database
 const MongoStore = require('connect-mongo');
 
@@ -27,6 +17,38 @@ const passport = require('passport');
 
 //session
 const session = require('express-session');
+
+var databaseName;
+
+app.use(async (req, res, next) => {
+  try {
+    await MongoClient.connect(MongoURI)
+      .then((db) => {
+        //database
+        console.log('db connected');
+        databaseName = db.db('todoapp');
+        app.databaseName = databaseName;
+      })
+      .catch((err) => {
+        return err;
+      });
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+//body-parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: '*', // 출처 허용 옵션
+    credential: 'true', // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+  }),
+);
 
 app.use(
   session({
@@ -38,53 +60,39 @@ app.use(
   }),
 );
 
-app.listen(8080, function () {
-  //server
-  console.log('listening on 8080');
+//passport
+require('./src/config/passport')(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
+//import routes /api/auth
+app.use('/api/auth', require('./src/api/auth.js'));
+//import routes /api/posts
+app.use('/api/', require('./src/api/posts.js'));
+app.use('/api/products', require('./src/api/products.js'));
+app.get('/', function (req, res) {
+  res.send('Hello');
 });
 
-MongoClient.connect(MongoURI)
-  .then((db) => {
-    //database
-    console.log('db connected');
-    var databaseName;
-    //
-    databaseName = db.db('todoapp');
-    app.databaseName = databaseName;
+// app.listen(8080, function () {
+//   //server
+//   console.log('listening on 8080');
+// });
 
-    //passport
-    require('./src/config/passport')(passport);
-    app.use(passport.initialize());
-    app.use(passport.session());
-
-    //import routes /api/auth
-    app.use('/api/auth', require('./src/api/auth.js'));
-    //import routes /api/posts
-    app.use('/api/', require('./src/api/posts.js'));
-    app.use('/api/products', require('./src/api/products.js'));
-    app.get('/', function (req, res) {
-      res.send('Hello');
-    });
-  })
-  .catch((err) => {
-    return err;
-  });
-
-// MongoClient.connect(MongoURI, (err, result) => {
-//   if (err) {
-//     return err;
-//   } else if (result) {
-//     console.log('db connected');
+// MongoClient.connect(MongoURI)
+//   .then((db) => {
 //     //database
-//     //awaiut function
-//     app.listen(8080, function () {
-//       console.log('listening on 8080');
-//     });
+//     console.log('db connected');
 //     var databaseName;
 //     //
-//     databaseName = result.db('todoapp');
+//     databaseName = db.db('todoapp');
 //     app.databaseName = databaseName;
-//   }
-// });
+//   })
+//   .catch((err) => {
+//     return err;
+//   });
+app.listen(8080, function () {
+  console.log('listening on 8080');
+});
 
 module.exports.handler = serverless(app);
