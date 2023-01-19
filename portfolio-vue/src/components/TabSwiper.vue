618559a13f3c2ae802200bc392cd5fcdd694a39b
andreas <andshmoon@gmail.com>
2023-01-19 15:16:50 +0900

    footer example

--- portfolio-vue/src/components/TabSwiper.vue
+++ portfolio-vue/src/components/TabSwiper.vue
@@ -1,16 +1,6 @@
 <template>
-  <section class="swiper-container">
-    <h5>클릭 수정해야함 $push로</h5>
-    <Swiper
-      class="swiper-wrapper"
-      :slides-per-view="2.5"
-      :space-between="6"
-      :slides-offset-after="0"
-      :slides-offset-before="0"
-      :free-mode="true"
-      :center-insufficient-slides="true"
-      role="tablist"
-    >
+  <section>
+    <Swiper :slides-per-view="3" :space-between="6" role="tablist">
       <SwiperSlide class="SwiperSlide" role="tab">
         <router-link to="/categories/electronics">electronics</router-link>
       </SwiperSlide>
@@ -45,56 +35,19 @@ import 'swiper/css';
 <style lang="scss" scoped>
 a {
   text-decoration: none;
-  color: skyblue;
+  color: white;
 }
 
-.swiper-container {
-  padding: 0 20px;
-  &:before,
-  &:after {
-    display: block;
-    position: absolute;
-    top: 0;
-    width: 20px;
-    height: 100%;
-    z-index: 10;
-    content: '';
-  }
-  &:before {
-    left: 0;
-    background: linear-gradient(
-      90deg,
-      #fff -20.19%,
-      rgba(255, 255, 255, 0.8) 18.31%,
-      rgba(255, 255, 255, 0) 75%
-    );
-  }
-  &:after {
-    right: 0;
-    background: linear-gradient(
-      270deg,
-      #fff -20.19%,
-      rgba(255, 255, 255, 0.8) 18.31%,
-      rgba(255, 255, 255, 0) 75%
-    );
-  }
-}
 .SwiperSlide {
-  width: auto;
-  min-width: 56px;
-  padding: 0px 14px;
   font-size: 14px;
   line-height: 36px;
   text-align: center;
-  color: #84868c;
+  color: white;
   border: 0;
   border-radius: 18px;
-  background: #f3f4f7;
+  background: #c9974d;
+  font-weight: 500;
   appearance: none;
   cursor: pointer;
-  &[aria-selected='true'] {
-    color: #fff;
-    background: #000;
-  }
 }
 </style>
